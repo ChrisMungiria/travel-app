50113ecd8aff4a9fd6d94f97bbd7bf84d26516ac
ChrisMungiria <christopher.mungiria@strathmore.edu>
2023-12-03 16:36:29 +0300

    Added Side Nav Bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { NAV_LINKS } from "@/constants";
@@ -30,12 +31,63 @@ const Navbar = () => {
       </div>
       <Image
         src="/menu.svg"
+        id="menu"
         alt="menu"
         width={24}
         height={24}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={() => {
+          const sideNavBar = document.getElementById("side-nav-bar");
+          sideNavBar?.classList.toggle("hidden");
+        }}
       />
+      <SideNavBar />
     </nav>
+  );
+};
+
+const SideNavBar = () => {
+  return (
+    <div
+      id="side-nav-bar"
+      className="hidden fixed top-0 right-0 h-screen w-80 bg-white z-40 flex flex-col gap-12 p-8"
+    >
+      <div className="flex justify-end">
+        <Image
+          src="/close.svg"
+          id="close"
+          alt="close"
+          width={24}
+          height={24}
+          className="inline-block cursor-pointer bg-green-90 rounded-full"
+          onClick={() => {
+            const sideNavBar = document.getElementById("side-nav-bar");
+            sideNavBar?.classList.toggle("hidden");
+          }}
+        />
+      </div>
+      <ul className="flex flex-col gap-12">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            onClick={() => {
+              const sideNavBar = document.getElementById("side-nav-bar");
+              sideNavBar?.classList.toggle("hidden");
+            }}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <Button
+        type="button"
+        title="Login"
+        icon="/user.svg"
+        variant="btn_dark_green"
+      />
+    </div>
   );
 };
 
